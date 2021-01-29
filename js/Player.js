0245e25e72e8fdd7f887272d7f54170a8aaffbfd
Shilpa <56382652+shilparvind@users.noreply.github.com>
2021-01-29 15:19:01 +0530

    Add files via upload

---
+++ js/Player.js
@@ -0,0 +1,50 @@
+class Player {
+  constructor() {
+    this.index = null;
+    this.distance = 0;
+    this.name = null;
+    this.rank = 0;
+  }
+
+  getCount() {
+    var playerCountRef = database.ref("playerCount");
+    playerCountRef.on("value", (data) => {
+      playerCount = data.val();
+    });
+  }
+
+  updateCount(count) {
+    database.ref("/").update({
+      playerCount: count,
+    });
+  }
+  getfinishedPlayers() {
+    var finishedPlayersref = database.ref("finishedPlayers");
+    finishedPlayersref.on("value", (data) => {
+      finishedPlayers = data.val();
+    });
+  }
+
+  static updatefinishedPlayers() {
+    database.ref("/").update({
+      finishedPlayers: finishedPlayers + 1,
+    });
+    this.rank = this.rank + 1;
+  }
+
+  update() {
+    var playerIndex = "players/player" + this.index;
+    database.ref(playerIndex).set({
+      name: this.name,
+      distance: this.distance,
+      rank: this.rank,
+    });
+  }
+
+  static getPlayerInfo() {
+    var playerInfoRef = database.ref("players");
+    playerInfoRef.on("value", (data) => {
+      allPlayers = data.val();
+    });
+  }
+}
